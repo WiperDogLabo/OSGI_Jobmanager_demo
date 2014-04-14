cdfad390ba97cf7db2494933d3fc92d2f11f396b
huongdt <huongdt@huongdt-vmware.(none)>
2014-04-14 17:29:31 +0700

    update to reproduce #373 note 8

--- FullOSGIWithDirectoryWatcher/lib/groovy/libs.common/JobLoader.groovy
+++ FullOSGIWithDirectoryWatcher/lib/groovy/libs.common/JobLoader.groovy
@@ -20,15 +20,48 @@ class JobLoader implements Listener{
 	
 	def processJob(jobfile){
 		def jobname = jobfile.getName().replace(".job", "")
-		def job = new CustomJob(jobname)
+		def job = new CustomJob(jobname, jobfile)
 		jf.createJob(job)
 	}
 	
 	def processTrg(trgfile){
-		def jobname = trgfile.getName().replace(".trg", "")
+		/*def jobname = trgfile.getName().replace(".trg", "")
 		def trg = jf.createTrigger(jobname, 0, 2000)
 		def jd = jf.getJob(jobname)
-		jf.scheduleJob(jd, trg)
+		jf.scheduleJob(jd, trg)*/
+		def shell = new GroovyShell()
+		trgfile.eachLine { aline -> 
+			def trg = shell.evaluate( "[" + aline + "]")
+			if (trg != null) {
+				def jobname = trg["job"]
+				def jd = jf.getJob(jobname)
+				def schedule = trg["schedule"]
+				
+				def trigger
+				if (schedule ==~ /[^ ]+[ ]+[^ ]+[ ]+[^ ]+[ ]+[^ ]+[ ]+[^ ]+[ ]+[^ ]+/ ||
+					schedule ==~ /[^ ]+[ ]+[^ ]+[ ]+[^ ]+[ ]+[^ ]+[ ]+[^ ]+[ ]+[^ ]+[ ]+[^ ]+/ ) {
+					// create trigger.
+					// created trigger will be registered automatically.
+					trigger = jf.createTrigger(jobname, schedule)
+				} else if (schedule == "now" || schedule == "NOW") {
+					// create trigger.
+					// created trigger will be registered automatically.
+					trigger = jf.createTrigger(jobname, 0)
+				} else if (schedule.endsWith('i')) {
+					long interval = Long.parseLong(schedule.substring(0,schedule.lastIndexOf('i')))*1000
+					trigger = jf.createTrigger(jobname, 0, interval)
+				}else if (schedule == 'delete'){
+					trigger = jf.getTrigger(jobname)
+					if(trigger != null){
+						jf.unscheduleJob(trigger)
+					}
+				}else {
+					long delay = Long.parseLong(schedule)
+					trigger = jf.createTrigger(jobname, delay)
+				}
+				jf.scheduleJob(jd, trigger)
+			}
+		}
 	}
 	
 	public boolean filterFile(File arg0) {
@@ -53,6 +86,7 @@ class JobLoader implements Listener{
 	}
 
 	public boolean notifyModified(File arg0) throws IOException {
+		processFile(arg0)
 		return true;
 	}
 }
